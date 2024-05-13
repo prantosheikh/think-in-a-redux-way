6627c96050492e41baf45dc72ebc3d6058bf5404
Pranto Sheikh <sheib-git>
2024-05-14 00:34:26 +0600

    Redux hook - useSelector

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
-import Counter from "./components/Counter";
+// import Counter from "./components/Counter";
 import { Provider } from "react-redux";
 import store from "./redux/store";
+import HooksCounter from "./components/HooksCounter";
 export default function App() {
    return (
       <Provider store={store}>
@@ -10,10 +11,10 @@ export default function App() {
             </h1>
 
             <div className="max-w-md mx-auto mt-10 space-y-5">
-               <Counter id='1' />
+               <HooksCounter id='1' />
 
             </div>
-         </div> 
+         </div>
       </Provider>
    );
 }
