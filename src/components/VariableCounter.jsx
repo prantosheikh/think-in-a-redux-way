28cc7322b8919c25da04be71ceea37f899a7a4c7
Pranto Sheikh <sheib-git>
2024-05-15 00:35:42 +0600

    sd

--- src/components/VariableCounter.jsx
+++ src/components/VariableCounter.jsx
@@ -1,39 +1,43 @@
-import { useDispatch, useSelector } from "react-redux";
+import { connect } from "react-redux";
 import { decrement, increment } from "../redux/counter/actions";
+import { decrement as dynamicDecrement, increment as dynamicIncrement } from "../redux/dynamicCounter/actions";
 
-// eslint-disable-next-line react/prop-types, no-unused-vars
-function HooksCounter() {
-	const count = useSelector((state) => state.counter.value);
-	console.log(count);
-	const dispatch = useDispatch();
-
-	const incrementHandler = () => {
-		dispatch(increment());
-	};
-
-	const decrementHandler = () => {
-		dispatch(decrement());
-	};
-
-	return (
-		<div className="p-4 h-auto flex flex-col items-center justify-center space-y-5 bg-white rounded shadow">
-			<div className="text-2xl font-semibold">{count}</div>
-			<div className="flex space-x-3">
-				<button
-					className="bg-indigo-400 text-white px-3 py-2 rounded shadow"
-					onClick={incrementHandler}
-				>
-					Increment
-				</button>
-				<button
-					className="bg-red-400 text-white px-3 py-2 rounded shadow"
-					onClick={decrementHandler}
-				>
-					Decrement
-				</button>
-			</div>
-		</div>
-	);
+// eslint-disable-next-line react/prop-types
+function VariableCounter({ count, increment, decrement }) {
+   return (
+      <div className="p-4 h-auto flex flex-col items-center justify-center space-y-5 bg-white rounded shadow">
+         <div className="text-2xl font-semibold">{count}</div>
+         <div className="flex space-x-3">
+            <button
+               className="bg-indigo-400 text-white px-3 py-2 rounded shadow"
+               onClick={increment}
+            >
+               Increment
+            </button>
+            <button
+               className="bg-red-400 text-white px-3 py-2 rounded shadow"
+               onClick={decrement}
+            >
+               Decrement
+            </button>
+         </div>
+      </div>
+   );
 }
 
-export default HooksCounter;
+const mapStateToProps = (state, ownProps) => {
+
+   return {
+      count: ownProps.dynamic ? state.dynamicCounter.value : state.counter.value,
+
+   };
+};
+
+const mapDispatchToProps = (dispatch, ownProps) => {
+   return {
+      increment: ownProps.dynamic ? () => dispatch(dynamicIncrement(5)) : () => dispatch(increment()),
+      decrement: ownProps.dynamic ? () => dispatch(dynamicDecrement(2)) : () => dispatch(increment()),
+   };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(VariableCounter);
