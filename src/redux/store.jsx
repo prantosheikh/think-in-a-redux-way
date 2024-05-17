01fd7623e75b7067ac66df04708988cc8ecda72b
Pranto sheikh <prantosheikh834@gmail.com>
2024-05-17 11:25:14 +0600

    Adding multiple middleware

--- src/redux/store.jsx
+++ src/redux/store.jsx
@@ -1,6 +1,8 @@
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
 import rootReducer from "./rootReducer";
+import myLogger from "./middleware/myLogger";
+import logger from "redux-logger";
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, applyMiddleware(logger, myLogger));
 
 export default store;
